147112ef7da15f6478e07b9cb7a72f46c41e2dc2
Christian Stankowic <info@cstan.io>
2019-12-01 14:01:11 +0100

    add unit tests (see also issue #7)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,15 +1,51 @@
+"""
+Molecule unit tests
+"""
 import os
-
 import testinfra.utils.ansible_runner
 
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+TESTINFRA_HOSTS = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
 
+def test_packages(host):
+    """
+    get variables from file
+    """
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    print("***")
+    print(ansible_vars)
+    # depend + uyuni
+    for pkg in ansible_vars["ansible_facts"]["core_packages"] + \
+        ansible_vars["ansible_facts"]["uyuni_packages"]:
+        assert host.package(pkg).is_installed
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_setup_complete(host):
+    """
+    check if installation files exist
+    """
+    with host.sudo():
+        for file in [
+                "/root/.MANAGER_SETUP_COMPLETE",
+                "/root/.MANAGER_INITIALIZATION_COMPLETE"]:
+            assert host.file(file).exists
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_ports_listen(host):
+    """
+    check if ports are listening
+    """
+    for port in [22, 80, 443, 4505, 4506]:
+        assert host.socket("tcp://0.0.0.0:%s" % port).is_listening
+
+def test_org(host):
+    """
+    get variables from file
+    """
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    # check if organization exists
+    cmd_org = host.run(
+        "spacecmd -q -u %s -p %s org_list",
+        ansible_vars["ansible_facts"]["org_login"],
+        ansible_vars["ansible_facts"]["org_password"]
+        )
+    assert cmd_org.stdout.strip() == ansible_vars["ansible_facts"]["org_name"]
