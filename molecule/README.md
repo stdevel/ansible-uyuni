584720d3fa6f4c02150fc2a94b9b591535ae33da
Christian Stankowic <christian.stankowic@sva.de>
2020-03-16 20:04:46 +0100

    split test in two scenarios

--- molecule/README.md
+++ molecule/README.md
@@ -5,7 +5,32 @@ This folder contains molecule configuration and tests.
 Ensure to the following installed:
 - [Vagrant](https://vagrantup.com)
 - [Oracle VirtualBox](https://virtualbox.org)
-- Molecule pip module
+- Python modules
+  - [`molecule`](https://pypi.org/project/molecule/)
+  - [`molecule-vagrant`](https://pypi.org/project/molecule-vagrant/)
+  - [`python-vagrant`](https://pypi.org/project/python-vagrant/)
+
+## Environment
+The test environment consists of two test scenarios:
+- `default` - default scenario with VM running openSUSE Leap 15.1
+- `suma` - SUSE Manager 4.x scenario with VM running SUSE Linux Enterprise Server 15 SP1
+
+### SUSE licensing hints
+In order to run tests against SUSE Linux Enterprise Server 15 SP1 / SUSE Manager 4.x you will either require a valid subscription or a trial license.
+You can request a [60-day trial on the SUSE website.](https://www.suse.com/products/suse-manager/download/)
+For this, you will need to create a [SUSE Customer Center](https://scc.suse.com) account - you will **not** be able to request an additional trial for the same release after the 60 days have expired.
+
+When using SLES, alter ``suma/converge.yml`` like this:
+```
+---
+- name: Converge machines
+  hosts: all
+  roles:
+    - role: ansible-uyuni
+      scc_reg_code: <insert code here>
+      scc_mail: <insert SCC mail here>
+...
+```
 
 ## Usage
 In order to create the test environment execute the following command:
@@ -13,9 +38,6 @@ In order to create the test environment execute the following command:
 ```
 $ molecule create
 ```
-
-After that, shutdown the VM and edit it. Add a second disk with **at least 40 GB** to the first controller.
-Power-on the VM again.
 
 Run the Ansible role:
 ```
@@ -26,6 +48,12 @@ Finally, run the tests:
 ```
 $ molecule verify
 ...
-    ===== 5 passed in 24.69 seconds =====
+collected 8 items
+
+    tests/test_default.py ........                                           [100%]
+
+    ========================== 8 passed in 14.09 seconds ===========================
 Verifier completed successfully.
 ```
+
+For running tests in the `suma` scenario context, run the commands above with the `-s suma` parameter.
---
+++ molecule/suma/tests/test_default.py
@@ -0,0 +1,139 @@
+"""
+Molecule unit tests
+"""
+import os
+import configparser
+import testinfra.utils.ansible_runner
+
+TESTINFRA_HOSTS = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_lvm(host):
+    """
+    test if storage was set-up correctly
+    """
+    # get variables from file
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    if ansible_vars["ansible_facts"]["use_lvm"]:
+        # check file systems
+        for filesys in ansible_vars["ansible_facts"]["filesystems"]:
+            assert host.mount_point(filesys["mountpoint"]).exists
+            assert host.mount_point(
+                filesys["mountpoint"]
+                ).filesystem == filesys["type"]
+
+
+def test_packages(host):
+    """
+    check if packages are installed
+    """
+    # get variables from file
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    # check dependencies and Uyuni packages
+    for pkg in ansible_vars["ansible_facts"]["core_packages"] + \
+            ansible_vars["ansible_facts"]["uyuni_packages"]:
+        assert host.package(pkg).is_installed
+
+
+def test_setup_complete(host):
+    """
+    check if installation files exist
+    """
+    with host.sudo():
+        for state_file in [
+                "/root/.MANAGER_SETUP_COMPLETE",
+                "/root/.MANAGER_INITIALIZATION_COMPLETE"]:
+            assert host.file(state_file).exists
+
+
+def test_ports_listen(host):
+    """
+    check if ports are listening
+    """
+    for port in [22, 80, 443, 4505, 4506]:
+        assert host.socket("tcp://0.0.0.0:%s" % port).is_listening
+
+
+def test_firewall(host):
+    """
+    check if firewall is configured properly
+    """
+    # get variables from file
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    # check if services are enabled
+    if ansible_vars["ansible_facts"]["config_firewall"]:
+        with host.sudo():
+            cmd_fw = host.run("firewall-cmd --list-services")
+            for srv in ansible_vars["ansible_facts"]["firewall_services"]:
+                assert srv in cmd_fw.stdout.strip()
+
+
+def test_org(host):
+    """
+    check if organization is accessible
+    """
+    # get variables from file
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    # check if organization exists
+    cmd_org = host.run(
+        "spacecmd -q -u %s -p %s org_list",
+        ansible_vars["ansible_facts"]["org_login"],
+        ansible_vars["ansible_facts"]["org_password"]
+        )
+    assert cmd_org.stdout.strip() == ansible_vars["ansible_facts"]["org_name"]
+
+
+def test_errata(host):
+    """
+    check if CEFS/DEFS are installed properly
+    """
+    # get variables from file
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    if ansible_vars["ansible_facts"]["setup_cefs"]:
+        # check package dependencies
+        for pkg in ansible_vars["ansible_facts"]["cefs_packages"]:
+            assert host.package(pkg).is_installed
+        # check script
+        assert host.file(
+            "{}/errata-import.pl" % ansible_vars["ansible_facts"]["cefs_path"]
+            ).exists
+        # check cronjobs
+        if ansible_vars["ansible_facts"]["setup_cefs_cronjob"]:
+            assert host.file("/etc/cron.d/errata-cefs").exists
+        if ansible_vars["ansible_facts"]["setup_defs_cronjob"]:
+            assert host.file("/etc/cron.d/errata-defs").exists
+
+
+def test_channels(host):
+    """
+    check if supplied channels were created
+    """
+    # get variables from file
+    ansible_vars = host.ansible("include_vars", "file=main.yml")
+    # get spacewalk-common-channels definitions from client
+    with host.sudo():
+        definition_file = host.file(
+            "/etc/rhn/spacewalk-common-channels.ini"
+            ).content_string
+    definitions = configparser.RawConfigParser(allow_no_value=True)
+    definitions.read_string(definition_file)
+
+    # check channels if defined
+    if len(ansible_vars["ansible_facts"]["channels"]) > 0:
+        # get all repositories
+        with host.sudo():
+            cmd_channels = host.run(
+                "spacecmd -q -u %s -p %s repo_list",
+                ansible_vars["ansible_facts"]["org_login"],
+                ansible_vars["ansible_facts"]["org_password"]
+            )
+        for channel in ansible_vars["ansible_facts"]["channels"]:
+            # get repository name (it ain't nice, but it's honest work)
+            repo_name = definitions[channel["name"]]["name"]
+            repo_name = "External - %s" % repo_name.replace(
+                "%(arch)s", channel["arch"]
+                )
+            # ensure that repository exists
+            assert repo_name in cmd_channels.stdout.strip().split("\n")
